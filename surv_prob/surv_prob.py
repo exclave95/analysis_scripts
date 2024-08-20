CORRECTED how covariance values are presented and interpreted in the results output file. now the full covariance matrix is presented after the fitted parameters, followed by the listing of the calculated errors for each parameter. the calculation of those errors using the covariance matrix is carried out and explained in the function definition

surv_prob/surv_prob.py
@@ -117,7 +117,7 @@ def surv_prob_curve_fit():
         # and needed along with a to ensure y is ~= 1 when x = 0
     
     # give parameters global scope (so the code can recognise them when the function is called)
-    global popt, pcov, a, k, c, x_fitted, y_fitted, a_pcov, k_pcov, c_pcov
+    global popt, pcov, perr, a, k, c, x_fitted, y_fitted
 
     # define optimization parameters and their covariance coefficients
     popt, pcov = curve_fit(lambda t, a, k, c: a * (np.exp(-k * t)) + c, x, y)
@@ -127,14 +127,13 @@ def surv_prob_curve_fit():
     k = popt[1]
     c = popt[2]
 
-    # define covariance values for optimised a, b and c
-    a_pcov = pcov[0]
-    k_pcov = pcov[1]
-    c_pcov = pcov[2]
-
     # define fitted x and y
     x_fitted = np.linspace(np.min(x), np.max(x), 100)
     y_fitted = a * np.exp(-k * x_fitted) + c
+
+    # create array of the error values of each fitted parameter
+    # this is done by identifying the DIAGONAL values of the covariance matrix (popt), and then calculating their square root
+    perr = np.sqrt(np.diag(pcov))
     
     # LEGACY plotting code, kept from original curve_fit tutorial (link: HERE)
     # ax = plt.axes()
@@ -217,9 +216,11 @@ for i in sel:
         file.write('\n--------------------')
         file.write(f'\n{i}')
         file.write(f'\na = {a}\nk = {k}\nc = {c}')
-        file.write(f'\n\nCovariance values:')
-        file.write(f'\npcov(a) = {a_pcov}\npcov(k) = {k_pcov}\npcov(c) = {c_pcov}')
-  
+        file.write(f'\n\nCovariance matrix:')
+        file.write(f'\n{pcov}')
+        file.write(f'\n\nParameter error values:')
+        file.write(f'\na error = {perr[0]}\nk error = {perr[1]}\nc error = {perr[2]}')
+        
     # END OF MODIFICATION 3
 
     # plotting